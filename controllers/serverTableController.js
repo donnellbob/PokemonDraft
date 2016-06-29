5c96aec818d40df9f5db7447336bdd285300db63
donnellbob <konk8888@gmail.com>
2016-06-29 15:45:34 +1000

    Working lobby system
    User can join and create rooms. (Rooms are created in backend but have
    no visual representation)

--- controllers/serverTableController.js
+++ controllers/serverTableController.js
@@ -1,34 +1,20 @@
 var app = angular.module('pokemonDraft', ['ui.bootstrap.modal', 'ui.bootstrap.tpls']);
 app.controller('serverTableController', function($scope, $http, $modal) {
+	// server list
+	$scope.hostedGames = [];
+	$scope.usersID;
+
     $scope.openModal = function(item) {
         var modalInstance = $modal.open({
             templateUrl: 'views/joinGame.html',
             backdrop: 'static',
             controller: function($scope, $modalInstance, $sce, item) {
-                var clone = {};
-                angular.copy(item, clone);
-                $scope.clone = clone;
+            	$scope.clone = item;
                 $scope.close = function() {
                     $modalInstance.dismiss('cancel');
                 };
-                $scope.save = function() {
-                  angular.extend(item, clone);
-                  $modalInstance.close();
-                };
-
-                $scope.joinGame = function(id){
-                	console.log(id);
-                }
-            },
-            resolve: {
-                item: function() {
-                    return item;
-                }
             }
         });
-
-        console.log(item);
-
     }
 
 
@@ -36,32 +22,44 @@ app.controller('serverTableController', function($scope, $http, $modal) {
     socket.on('connect', function(data) {
     	// default namespace/room (its a lobby)
         socket.emit('initialConnection', 'New client has joined :)');
+
+        // Gets users socket ID
+        $scope.usersID = socket.io.engine.id
     });
 
 
-    //USED WHEN CREATING GAME :) THIS IS THE CLIENTS ID
-    var id = socket.io.engine.id
-	console.log(id); // returns undefined as its called before socket connection
-
-	// Testing array :)
-	$scope.hostedGames = [
-	{name: "Greg", hostName: "George", players: "1/4", private: "No", id: '1'}, 
-	{name: "Cool Game", hostName: "Jim", players: "1/2", private: "Yes", id: '2'},
-	{name: "Fun Game", hostName: "Tim", players: "1/3", private: "No", id: '3'},
-	{name: "Crazy Game", hostName: "Old'greg", players: "1/5", private: "Yes", id: '4'},
-	{name: "Interesting Game", hostName: "Jay", players: "1/4", private: "Yes", id: '5'}
-	];
-
-
 	$scope.joinGame = function(id){
-		console.log("REACHED");
 		socket.emit('joinGame', id);
-		console.log(id);
+		console.log("Joining room ID: " + id);
 	}
 
 	socket.on('message', function(data) {
 	   console.log('Success:', data);
 	});
+
+	$http.get("/model/serverTable").then(function(response) {
+        $scope.hostedGames = response.data;
+    });
+
+    $scope.refreshServerTable = function(){
+    	$http.get("/model/serverTable").then(function(response) {
+	        $scope.hostedGames = response.data;
+	    });
+    }
+
+    $scope.createGame = function(id, gameName, name, password){
+    	var newGame = {name: gameName, hostName: name, players: 1, private: "No", id: id};
+	   $http.post("/model/serverTableUpdate", newGame)
+	    .success(
+        function(success){
+            console.log(success)
+        })
+   		.error(
+        function(error){
+            console.log(error)
+        });
+
+   	}
 
 });
 
--- model/serverSelect.js
+++ model/serverSelect.js
@@ -1,5 +1,14 @@
 module.exports = function(app, io){
 
+	//Testing values
+	var hostedGames = [
+	{name: "Greg", hostName: "George", players: 2, private: "No", id: '1'}, 
+	{name: "Cool Game", hostName: "Jim", players: 1, private: "Yes", id: '2'},
+	{name: "Fun Game", hostName: "Tim", players: 1, private: "No", id: '3'},
+	{name: "Crazy Game", hostName: "Old'greg", players: 2, private: "Yes", id: '4'},
+	{name: "Interesting Game", hostName: "Jay", players: 1, private: "Yes", id: '5'}
+	];
+
 	io.on('connection', function(client) {  
 	    console.log('Client connected...');
 
@@ -8,14 +17,20 @@ module.exports = function(app, io){
 	    });
 
 	    client.on('joinGame', function(data) {
-	        console.log(data);
 	        client.join(data);
-	        io.sockets.in('1').emit('message', 'Successfully joined room');
+	        io.sockets.in(data).emit('message', 'Successfully joined room' + data);
 	    });
+	});
 
-	    io.sockets.in(1).emit('message', 'anyone in this room yet?');
-	    io.sockets.in('foobar').emit('message', 'anyone in this room yet?');
 
+	// Returns server list
+	app.get("/model/serverTable", function(req, res){
+		res.send(hostedGames);
+	});
+
+	//Adds new server to list
+	app.post("/model/serverTableUpdate", function(req, res){
+		hostedGames.push(req.body);
 	});
 
 }
